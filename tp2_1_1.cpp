35b34934e964a8978a7fd370ae2aea1b67eeb2de
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-04-06 22:00:34 -0300

    Primer punto

---
+++ tp2_1_1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define N 4
+#define M 5
+
+int main(int argc, char const *argv[])
+{
+    
+   
+int f,c;
+double mt[N][M];
+  printf("HOLA\n");
+  
+for(f = 0;f<N; f++)
+{
+ for(f = 0;f<N; f++){
+    
+    printf("%lf", mt[f][c]);
+}
+    printf("HOLA\n");
+}
+ return 0;
+
+}
